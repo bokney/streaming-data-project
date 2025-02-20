511e9243ab3241e40611ddac937a3604d52da3b7
bokney <toknyk@gmail.com>
2025-02-20 13:52:32 +0000

    removed prefixes and module names

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'streaming data project'
-copyright = '2025, dungo_squart'
-author = 'dungo_squart'
+copyright = '2025, bokney'
+author = 'bokney'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
